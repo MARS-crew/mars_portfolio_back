11350c781dac70fd2e6b0eed8edf34539ba7e797
ssuj <dbsdmsdud912@gmail.com>
2023-06-28 00:45:37 +0900

    소셜로그인(네이버, 구글, 카카오, 페이스북) 1차 개발 완료

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,5 +1,8 @@
 const passport = require('passport');
 const NaverStrategy = require('passport-naver').Strategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const KakaoStrategy = require('passport-kakao').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const db = require('./db');
 
 // Passport 직렬화
@@ -18,40 +21,58 @@ passport.use(new NaverStrategy({
   clientSecret: '5souQXoeSG',
   callbackURL: '/auth/naver/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  // 네이버 로그인 성공 시 호출되는 함수
-  const data = {
-    naverId: profile.id,
-    type: 1,
-    name: 'name',//profile.displayName,
-    email: profile.emails[0].value
-  };
   console.log(profile);
-  console.log(data);
+
+  // 네이버 로그인 성공 시 호출되는 함수
+  const email = profile.emails[0].value;
+  const type = profile.provider;
+
   // 데이터베이스에 이미 등록된 사용자인지 확인
-  let sql = `SELECT * FROM tbl_sns WHERE NAME = ? AND TYPE = ?`;
-  db.query(sql, [data.naverId, data.type], function(err,rows) {
+  let sql = `
+    SELECT * FROM tbl_sns sn 
+    WHERE sn.type = ?
+    AND sn.member_id = 
+    (SELECT mb.member_id FROM tbl_member mb WHERE mb.email = ?);
+  `;
+
+  db.query(sql, [type, email], (err, rows) => {
     if (err) return done(err);
 
     // 이미 등록된 사용자인 경우
     if (rows.length > 0) {
-        return done(null, rows[0]);
+      return done(null, rows[0]);
+    }
+
+    let data1 = {};
+    data1.email = email;
+    if(profile.displayName != undefined) {
+      data1.name = profile.displayName;
     }
 
     // 새로운 사용자인 경우, 데이터베이스에 저장
-    sql = `INSERT INTO tbl_member (email, name, tel, birth) VALUES (?, ?, ?, ?)`;
-    db.query(sql, [data.email, data.name, '010', '20000712'], function(err,rows) {
+    sql = `INSERT INTO tbl_member SET ?`;
+    db.query(sql, data1, (err, rows) => {
       if (err) return done(err);
 
       if (rows.affectedRows > 0) {
-        const member_id = rows.insertId;
-        sql = `INSERT INTO tbl_sns (member_id, type, name, access_token, refresh_token) VALUES (?, ?, ?, ?, ?)`;
-        db.query(sql, [member_id, data.type, data.naverId, accessToken, refreshToken], function(err,rows) {
+        let data2 = {};
+        data2.member_id = rows.insertId;
+        data2.access_token = accessToken;
+        data2.refresh_token = refreshToken;
+        data2.type = type;
+
+        if(profile._json.nickname != undefined) {
+          data2.name = profile._json.nickname;
+        }
+
+        sql = `INSERT INTO tbl_sns SET ?`;
+        db.query(sql, data2, (err, rows) => {
           if (err) return done(err);
-  
+
           if (rows.affectedRows > 0) {
             // 데이터베이스에서 삽입된 사용자 가져오기
             sql = `SELECT * FROM tbl_member WHERE member_id = ?`;
-            db.query(sql, [member_id], (err, rows) => {
+            db.query(sql, [data2.member_id], (err, rows) => {
               if (err) return done(err);
               return done(null, rows[0]);
             });
@@ -61,5 +82,215 @@ passport.use(new NaverStrategy({
     });
   });
 }));
+
+// Google Strategy 설정
+passport.use(new GoogleStrategy({
+  clientID: '101139357605-p8b2dk8opivcm0vvrecm1rb6ci7los03.apps.googleusercontent.com',
+  clientSecret: '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',
+  callbackURL: '/auth/google/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+
+  // 구글 로그인 성공 시 호출되는 함수
+  const email = profile.emails[0].value;
+  const type = profile.provider;
+
+  // 데이터베이스에 이미 등록된 사용자인지 확인
+  let sql = `
+    SELECT * FROM tbl_sns sn 
+    WHERE sn.type = ?
+    AND sn.member_id = 
+    (SELECT mb.member_id FROM tbl_member mb WHERE mb.email = ?);
+  `;
+  
+  db.query(sql, [type, email], (err, rows) => {
+    if (err) return done(err);
+
+    // 이미 등록된 사용자인 경우
+    if (rows.length > 0) {
+      return done(null, rows[0]);
+    }
+
+    let data1 = {};
+    data1.email = email;
+    if(profile.displayName != undefined) {
+      data1.name = profile.displayName;
+    }
+
+    // 새로운 사용자인 경우, 데이터베이스에 저장
+    sql = `INSERT INTO tbl_member SET ?`;
+    db.query(sql, data1, (err, rows) => {
+      if (err) return done(err);
+
+      if (rows.affectedRows > 0) {
+        let data2 = {};
+        data2.member_id = rows.insertId;
+        data2.access_token = accessToken;
+        data2.refresh_token = refreshToken;
+        data2.type = type;
+
+        if(profile.displayName != undefined) {
+          data2.name = profile.displayName;
+        }
+
+        sql = `INSERT INTO tbl_sns SET ?`;
+        db.query(sql, data2, (err, rows) => {
+          if (err) return done(err);
+
+          if (rows.affectedRows > 0) {
+            // 데이터베이스에서 삽입된 사용자 가져오기
+            sql = `SELECT * FROM tbl_member WHERE member_id = ?`;
+            db.query(sql, [data2.member_id], (err, rows) => {
+              if (err) return done(err);
+              return done(null, rows[0]);
+            });
+          }
+        });
+      }
+    });
+  });
+}));
+
+
+// 카카오톡 로그인 설정
+passport.use(new KakaoStrategy({
+  clientID: '38cc3b67106247149a1827fde93259bf',
+  clientSecret: 'ExlAvepfPoNyDmzWzJUfMLVA53eWfW86',
+  callbackURL: '/auth/kakao/callback',
+}, (accessToken, refreshToken, profile, done) => {
+  console.log(profile);
+
+  // 카카오톡 로그인 성공 시 호출되는 함수
+  const email = profile._json.kakao_account.email;
+  const type = profile.provider;
+
+  // 데이터베이스에 이미 등록된 사용자인지 확인
+  let sql = `
+    SELECT * FROM tbl_sns sn 
+    WHERE sn.type = ?
+    AND sn.member_id = 
+    (SELECT mb.member_id FROM tbl_member mb WHERE mb.email = ?);
+  `;
+
+  db.query(sql, [type, email], (err, rows) => {
+    if (err) return done(err);
+
+    // 이미 등록된 사용자인 경우
+    if (rows.length > 0) {
+      return done(null, rows[0]);
+    }
+
+    let data1 = {};
+    data1.email = email;
+    if (profile.username != undefined) {
+      data1.name = profile.username;
+    }
+
+    // 새로운 사용자인 경우, 데이터베이스에 저장
+    sql = `INSERT INTO tbl_member SET ?`;
+    db.query(sql, data1, (err, rows) => {
+      if (err) return done(err);
+
+      if (rows.affectedRows > 0) {
+        let data2 = {};
+        data2.member_id = rows.insertId;
+        data2.access_token = accessToken;
+        data2.refresh_token = refreshToken;
+        data2.type = type;
+
+        if (profile.username != undefined) {
+          data2.name = profile.username;
+        }
+
+        sql = `INSERT INTO tbl_sns SET ?`;
+        db.query(sql, data2, (err, rows) => {
+          if (err) return done(err);
+
+          if (rows.affectedRows > 0) {
+            // 데이터베이스에서 삽입된 사용자 가져오기
+            sql = `SELECT * FROM tbl_member WHERE member_id = ?`;
+            db.query(sql, [data2.member_id], (err, rows) => {
+              if (err) return done(err);
+              return done(null, rows[0]);
+            });
+          }
+        });
+      }
+    });
+  });
+}));
+
+// 페이스북 로그인 설정
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: '205344475801010',
+      clientSecret: '95642b0a74fb5d4c6825c9949bc332bd',
+      callbackURL: '/auth/facebook/callback',
+      profileFields: ['id', 'displayName', 'email'],
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(profile);
+
+      // 페이스북 로그인 성공 시 호출되는 함수
+      const email = profile.emails[0].value;
+      const type = profile.provider;
+
+      // 데이터베이스에 이미 등록된 사용자인지 확인
+      let sql = `
+        SELECT * FROM tbl_sns sn 
+        WHERE sn.type = ?
+        AND sn.member_id = 
+        (SELECT mb.member_id FROM tbl_member mb WHERE mb.email = ?);
+      `;
+  
+      db.query(sql, [type, email], (err, rows) => {
+        if (err) return done(err);
+
+        // 이미 등록된 사용자인 경우
+        if (rows.length > 0) {
+          return done(null, rows[0]);
+        }
+
+        let data1 = {};
+        data1.email = email;
+        if (profile.displayName != undefined) {
+          data1.name = profile.displayName;
+        }
+
+        // 새로운 사용자인 경우, 데이터베이스에 저장
+        sql = `INSERT INTO tbl_member SET ?`;
+        db.query(sql, data1, (err, rows) => {
+          if (err) return done(err);
+
+          if (rows.affectedRows > 0) {
+            let data2 = {};
+            data2.member_id = rows.insertId;
+            data2.access_token = accessToken;
+            data2.type = type;
+
+            if (profile.displayName != undefined) {
+              data2.name = profile.displayName;
+            }
+
+            sql = `INSERT INTO tbl_sns SET ?`;
+            db.query(sql, data2, (err, rows) => {
+              if (err) return done(err);
+
+              if (rows.affectedRows > 0) {
+                // 데이터베이스에서 삽입된 사용자 가져오기
+                sql = `SELECT * FROM tbl_member WHERE member_id = ?`;
+                db.query(sql, [data2.member_id], (err, rows) => {
+                  if (err) return done(err);
+                  return done(null, rows[0]);
+                });
+              }
+            });
+          }
+        });
+      });
+    }
+  )
+);
 
 module.exports = passport;
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -32,15 +32,25 @@ router.post(apiUrl + '/review/like', reviewCtrl.process['like']);
 // 회원
 const passport = require('../../config/passport');
 
+router.use(passport.initialize());
+router.use(passport.session());
+
 // 로그인 페이지 렌더링
 router.get('/login', (req, res) => {
   const loginButtonHtml = `
     <a href="/auth/naver">naver 로그인</a>
-    <a href="/auth/facebook">facebook 로그인</a>
+    <a href="/auth/kakao">kakao 로그인</a>
     <a href="/auth/google">google 로그인</a>
+    <a href="/auth/facebook">facebook 로그인</a>
   `;
   res.send(loginButtonHtml);
 });
+
+// 소셜 로그아웃 라우트
+router.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/login');
+  });
 
 // 소셜 로그인 라우트 (Naver)
 router.get('/auth/naver', passport.authenticate('naver'));
@@ -51,95 +61,38 @@ router.get('/auth/naver/callback', passport.authenticate('naver', {
   failureRedirect: '/login'
 }));
 
-// 소셜 로그아웃 라우트
-router.get('/logout', (req, res) => {
-  req.logout();
-  res.redirect('/login');
-});
+// 소셜 로그인 라우트 (Google)
+router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+// router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'], accessType: 'offline' }));
+
+// 소셜 로그인 콜백 라우트 (Google)
+router.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect: '/test',
+  failureRedirect: '/login'
+}));
+
+// 카카오톡 로그인
+router.get('/auth/kakao', passport.authenticate('kakao'));
+
+// 카카오톡 로그인 콜백
+router.get(
+  '/auth/kakao/callback',
+  passport.authenticate('kakao', {
+    successRedirect: '/test',
+    failureRedirect: '/login',
+  })
+);
+
+// 소셜 로그인 라우트 (Facebook)
+router.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email'] }));
+
+// 소셜 로그인 콜백 라우트 (Facebook)
+router.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/test',
+  failureRedirect: '/login'
+}));
 
 
 
-
-
-
-
-
-
-// router.get(apiUrl + '/user/login/naver', userCtrl.output['naver']);
-
-// const passport = require('passport');
-// const NaverStrategy = require('passport-naver').Strategy;
-
-// // 네이버 개발자센터에서 발급받은 클라이언트 ID와 시크릿을 입력합니다.
-// const NAVER_CLIENT_ID = 'OkrQLa31Si2PsDyhpSjx';
-// const NAVER_CLIENT_SECRET = '5souQXoeSG';
-
-// // 세션에 사용자 정보를 저장하기 위해 serialize와 deserialize 메서드를 설정합니다.
-// passport.serializeUser((user, done) => {
-//   done(null, user);
-// });
-
-// passport.deserializeUser((user, done) => {
-//   done(null, user);
-// });
-
-// // 네이버 로그인 전략을 설정합니다.
-// passport.use(
-//   new NaverStrategy(
-//     {
-//       clientID: NAVER_CLIENT_ID,
-//       clientSecret: NAVER_CLIENT_SECRET,
-//       callbackURL: '/api/v1/user/login/naver/callback', // 네이버 로그인 콜백 URL을 설정합니다.
-//     },
-//     (accessToken, refreshToken, profile, done) => {
-//       // 사용자 정보를 가져온 후 필요한 처리를 수행합니다.
-//       // 예시로 사용자 정보를 세션에 저장합니다.
-//       const user = {
-//         id: profile.id,
-//         name: profile.displayName,
-//         email: profile.emails[0].value,
-//       };
-//       return done(null, user);
-//     }
-//   )
-// );
-
-// // Express 앱에 Passport 미들웨어를 등록합니다.
-router.use(passport.initialize());
-router.use(passport.session());
-
-// router.get(apiUrl + '/user/login', (req, res) => {
-//   const loginButtonHtml = `
-//     <a href="/api/v1/user/login/naver">네이버로 로그인</a>
-//   `;
-//   res.send(loginButtonHtml);
-// });
-
-// // 네이버 로그인 요청을 처리하기 위한 라우트 핸들러입니다.
-// router.get(
-//   '/api/v1/user/login/naver',
-//   passport.authenticate('naver', { scope: ['profile', 'email'] })
-// );
-
-// // 네이버 로그인 콜백 핸들러입니다.
-// router.get(
-//   '/api/v1/user/login/naver/callback',
-//   passport.authenticate('naver', { failureRedirect: '/login' }),
-//   (req, res) => {
-//     // 인증이 완료되면 메인 페이지로 리다이렉트합니다.
-//     res.redirect('/user');
-//   }
-// );
-
-// // 사용자 정보 확인을 위한 라우트 핸들러입니다.
-// router.get('/user', (req, res) => {
-//   // 로그인된 사용자 정보를 가져옵니다.
-//   const user = req.user;
-//   if (user) {
-//     res.send(`Hello, ${user.name}!`);
-//   } else {
-//     res.send('Not logged in.');
-//   }
-// });
 
 module.exports = router;
